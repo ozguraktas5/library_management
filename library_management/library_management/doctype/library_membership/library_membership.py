5c56cbf1be2b4d87746dab9988baa005eab8f823
Özgür Aktaş <ozguraktas.55555@gmail.com>
2024-11-19 20:07:29 +0300

    son degisiklikler

--- library_management/library_management/doctype/library_membership/library_membership.py
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2024, ozguraktas and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
 
 
 class LibraryMembership(Document):
-	pass
+    
+    def before_submit(self):
+        exists = frappe.db.exists(
+            "Library Membership",
+            {
+                "library_member": self.library_member,
+                "docstatus": DocStatus.submitted(),
+                # check if the membership's end date is later than this membership's start date
+                "to_date": (">", self.from_date),
+            },
+        )
+        if exists:
+            frappe.throw("There is an active membership for this member")
