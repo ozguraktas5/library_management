582302a4a6b0afbbd2394f8973f44148e876c8fe
Özgür Aktaş <ozguraktas.55555@gmail.com>
2024-11-20 13:44:56 +0300

    son degisiklikler

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -7,6 +7,7 @@ class LibraryTransaction(Document):
     def before_submit(self):
         if self.type == "Issue":
             self.validate_issue()
+            self.validate_maximum_limit()
             # Set the article status to be Issued
             article = frappe.get_doc("Article", self.article)
             article.status = "Issued"
@@ -31,6 +32,19 @@ class LibraryTransaction(Document):
         # Article cannot be returned if it is not issued first
         if article.status == "Available":
             frappe.throw("Article cannot be returned without being issued first")
+
+    def validate_maximum_limit(self):
+        max_articles = frappe.db.get_single_value("Library Settings", "maximum_number_of_issued_articles")
+        count = frappe.db.count(
+            "Library Transaction",
+            {
+                "library_member": self.library_member,
+                "type": "Issue",
+                "docstatus": DocStatus.submitted(),
+            },
+        )
+        if count >= max_articles:
+            frappe.throw("Maximum limit reached for issuing articles")
 
     def validate_membership(self):
         # Check if a valid membership exists for this library member
